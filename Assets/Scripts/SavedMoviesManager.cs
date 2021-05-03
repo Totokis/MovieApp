48323eae5bfff9e4108b9de7c918bd675c2215a1
Totokis <pawelj_janusz@wp.pl>
2021-05-03 09:08:17 +0200

    Script for displaying list of user's movies

---
+++ Assets/Scripts/SavedMoviesManager.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+class SavedMoviesManager: MonoBehaviour
+{
+    List<Movie> _moviesList;
+    public void SetMovies(List<Movie> movies)
+    {
+        
+    }
+}
