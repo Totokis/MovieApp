92ea4eb2e295b9440c85747ed84e18dc25ade256
Totokis <pawelj_janusz@wp.pl>
2021-05-03 09:18:59 +0200

    Added handling for double click event

--- Assets/Scripts/SearchedItem.cs
+++ Assets/Scripts/SearchedItem.cs
@@ -1,35 +1,31 @@
-using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Networking;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-class SearchedItem : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler
+class SearchedItem : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
-    [SerializeField] TMP_Text _text;
-    [SerializeField] TMP_Text _releaseDate;
-    [SerializeField] Sprite _deafulSprite;
+    [SerializeField] TMP_Text text;
+    [SerializeField] TMP_Text releaseDate;
+    [SerializeField] Sprite deafulSprite;
     [SerializeField] Image image;
-    [SerializeField] Shader _shader; 
-    [SerializeField] Material _newMaterial;
+    [SerializeField] Shader shader; 
+    [SerializeField] Material newMaterial;
 
     SearchedMovie _movie;
-    Sprite spriteFromRequest; 
-    Texture texture;
-    private void Awake()
+    Sprite _spriteFromRequest; 
+    Texture _texture;
+    float _clicked = 0;
+    float _clicktime = 0;
+    float _clickdelay = 0.5f;
+    
+    void Awake()
     {
-        _newMaterial = new Material(_shader);
+        newMaterial = new Material(shader);
     }
-    public void SetMovie(SearchedMovie movie)
-    {
-        _movie = movie;
-        _text.text = _movie.title;
-        _releaseDate.text = _movie.releaseDate;
-        StartCoroutine(GetImage());
-    }
+    
     IEnumerator GetImage()
     {
         using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(_movie.imageUrl))
@@ -37,23 +33,31 @@ class SearchedItem : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPoint
              yield return uwr.SendWebRequest();
              if (uwr.result != UnityWebRequest.Result.Success)
              {
-                 spriteFromRequest = _deafulSprite;
+                 _spriteFromRequest = deafulSprite;
              }
              else
              {
-                 texture = DownloadHandlerTexture.GetContent(uwr);
+                 _texture = DownloadHandlerTexture.GetContent(uwr);
                  //spriteFromRequest = Sprite.Create(texture, new Rect(0f, 0f, texture.width, texture.height), Vector2.zero, 10f);
              }
         }
         SetImage();
     }
+    
     void SetImage()
     {
-        _newMaterial.SetTexture("_MainTexture",texture);
-        _newMaterial.SetFloat("_PixelateAmount",0.8f);
-        image.material = _newMaterial;
+        newMaterial.SetTexture("_MainTexture",_texture);
+        newMaterial.SetFloat("_PixelateAmount",0.8f);
+        image.material = newMaterial;
     }
     
+    public void SetMovie(SearchedMovie movie)
+    {
+        _movie = movie;
+        text.text = _movie.title;
+        releaseDate.text = _movie.releaseDate;
+        StartCoroutine(GetImage());
+    }
     public void OnSelect(BaseEventData eventData)
     {
         //image.material.SetFloat("_PixelateAmount",0f);
@@ -67,5 +71,27 @@ class SearchedItem : MonoBehaviour, ISelectHandler, IPointerEnterHandler, IPoint
     {
         Debug.Log("OnPointerExit");
         image.materialForRendering.SetFloat("_PixelateAmount",0.8f);
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        /*
+         * Code from here:
+         * https://forum.unity.com/threads/detect-double-click-on-something-what-is-the-best-way.476759/
+         */
+        _clicked++;
+        if (_clicked == 1) _clicktime = Time.time;
+ 
+        if (_clicked > 1 && Time.time - _clicktime < _clickdelay)
+        {
+            _clicked = 0;
+            _clicktime = 0;
+            Debug.Log("Double CLick: "+this.GetComponent<RectTransform>().name);
+ 
+        }
+        else if (_clicked > 2 || Time.time - _clicktime > 1)
+        {
+            _clicked = 0;
+            Debug.Log("One CLick: "+this.GetComponent<RectTransform>().name);
+        }
     }
 }
