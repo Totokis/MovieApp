349fdb8d662ce8273b3aca12085c3dece482224b
Totokis <pawelj_janusz@wp.pl>
2021-05-06 18:08:39 +0200

    Added new fields

--- Assets/Scripts/Movie.cs
+++ Assets/Scripts/Movie.cs
@@ -1,29 +1,42 @@
 using System.Runtime.InteropServices.ComTypes;
+using JetBrains.Annotations;
 
 public class Movie
 {
     string title;
     string imageUrl;
-    string description;
+    string releaseDate;
     bool seen;
+    string description;
+    string author;
+    string note;
 
     public string Title => title;
     public string ImageUrl => imageUrl;
-    public string Description => description;
+    public string ReleaseDate => releaseDate;
     public bool Seen => seen;
+    public string Description => description;
+    public string Author => author;
+    public string Note => note;
 
     public Movie()
     {
         title = "Not defined";
         imageUrl = "";
-        description = "No description";
+        releaseDate = "No release date";
         seen = false;
+        description = "No description";
+        author = "No author";
+        note = "No notes";
     }
-    public Movie(string title, string imageUrl, string description, bool seen)
+    public Movie(string title, string imageUrl, string releaseDate, bool seen)
     {
         this.title = title;
         this.imageUrl = imageUrl;
-        this.description = description;
+        this.releaseDate = releaseDate;
         this.seen = seen;
+        description = "No description";
+        author = "No author";
+        note = "No notes";
     }
 }
