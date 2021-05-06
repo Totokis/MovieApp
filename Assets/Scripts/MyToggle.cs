3c6c35c73705aee69693a16aa3187b013dee873a
Totokis <pawelj_janusz@wp.pl>
2021-05-06 18:09:35 +0200

    Fixed missing monobehaviour

--- Assets/Scripts/MyToggle.cs
+++ Assets/Scripts/MyToggle.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MyToggle : MonoBehaviour, IPointerClickHandler
+public class MyToggle: MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] SavedMovieItem savedMovieItem;
     public void OnPointerClick(PointerEventData eventData)
