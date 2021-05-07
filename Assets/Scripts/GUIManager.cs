8186b59f08d76a18f10f3f9e5405bd7d1adea8c1
Totokis <pawelj_janusz@wp.pl>
2021-05-07 15:37:18 +0200

    Added popup

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -6,8 +6,12 @@ public class GUIManager : MonoBehaviour
 {
    [SerializeField] UpdateMoviePanel updateMoviePanel;
    [SerializeField] GameObject savedMoviePanel;
+   [SerializeField] AddedPopUp addedPopUp;
 
    public UpdateMoviePanel UpdateMoviePanel => updateMoviePanel;
 
    public GameObject SavedMoviePanel => savedMoviePanel;
+   
+   public AddedPopUp AddedPopUp => addedPopUp;
+
 }
