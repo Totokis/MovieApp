5be0dfec3db2a26cc9aefb182f324ebe220dfe74
Totokis <pawelj_janusz@wp.pl>
2021-05-15 14:21:11 +0200

    Added TODO

--- Assets/Scripts/APIHelper.cs
+++ Assets/Scripts/APIHelper.cs
@@ -8,6 +8,7 @@ public static class APIHelper
 {
    public static List<Result> GetNewResult(string title="Shrek")
    {
+      //TODO Get API from firebase;
       HttpWebRequest request = WebRequest.Create($"https://api.themoviedb.org/3/search/movie?api_key=61f3437d009eac7fd9c283bc9bf1b3ad&language=en-US&query={title}&page=1&include_adult=false") as HttpWebRequest;
       HttpWebResponse response = request.GetResponse() as HttpWebResponse;
       StreamReader reader = new StreamReader(response.GetResponseStream());
